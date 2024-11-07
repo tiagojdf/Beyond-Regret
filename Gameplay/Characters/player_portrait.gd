16d344a6d13a496e51d790174f66bad12754367c
Tiago Ferreira <tiagojdferreira@gmail.com>
2024-11-07 23:07:14 +0100

    Adds a bit of logic to the game

--- Gameplay/Characters/player_portrait.gd
+++ Gameplay/Characters/player_portrait.gd
@@ -3,5 +3,14 @@ extends DialogicPortrait
 
 @onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
 
+var health_per_centage: float
+
+func _process(_delta: float) -> void:
+	if gpu_particles_2d == null:
+		return
+	
+	health_per_centage = Dialogic.VAR.values.health/100.
+	gpu_particles_2d.process_material.color = Color.RED.lerp(Color.GREEN, health_per_centage)
+
 func _get_covered_rect() -> Rect2:
 	return Rect2(gpu_particles_2d.position, Vector2(80, 80))
--- Gameplay/Levels/level_01.gd
+++ Gameplay/Levels/level_01.gd
@@ -1,9 +1,36 @@
 extends Node2D
 
+@onready var debug: Control = $Debug
+
+@onready var meaning_label: Label = $Debug/VBoxContainer/MeaningLabel
+@onready var connection_label: Label = $Debug/VBoxContainer/ConnectionLabel
+@onready var fulfillment_label: Label = $Debug/VBoxContainer/FulfillmentLabel
+@onready var legacy_label: Label = $Debug/VBoxContainer/LegacyLabel
+@onready var inner_peace_label: Label = $Debug/VBoxContainer/InnerPeaceLabel
+@onready var health_label: Label = $Debug/VBoxContainer/HealthLabel
+
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color(.05,.05,.1))
 	Dialogic.start('main')
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
+	Dialogic.VAR.variable_changed.connect(_on_variable_changed)
+	
+	# For development purposes
+	if OS.is_debug_build():
+		update_labels()
+		debug.visible = true
+
+func _on_variable_changed(_info: Dictionary) -> void:
+	update_labels()
+	
+func update_labels() -> void:
+	meaning_label.text = str("Meaning:", Dialogic.VAR.values.meaning)
+	connection_label.text = str("Connection:", Dialogic.VAR.values.connection)
+	fulfillment_label.text = str("Fulfillment:", Dialogic.VAR.values.fulfillment)
+	legacy_label.text = str("Legacy:", Dialogic.VAR.values.legacy)
+	inner_peace_label.text = str("Inner Peace:", Dialogic.VAR.values.inner_peace)
+	health_label.text = str("Health:", Dialogic.VAR.values.health)
 
 func _on_timeline_ended() -> void:
-	get_tree().quit()
+	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
+	SceneManager.swap_scenes(SceneRegistry.main_scenes["CreditsScreen"],get_tree().root,self,"wipe_to_right")	
