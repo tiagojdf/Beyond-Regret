2e82af3afc5c32b875a068298a3eb8c161e95f50
Tiago Ferreira <tiagojdferreira@gmail.com>
2024-11-09 23:52:15 +0100

    Added a million things
    Made portrait scale with health. Finished age 20. Started age 40. Opted for a positive message between ages. Added wheezing sound.

--- Gameplay/Characters/player_portrait.gd
+++ Gameplay/Characters/player_portrait.gd
@@ -5,17 +5,35 @@ extends DialogicPortrait
 
 var health_per_centage: float
 
+var original_process_material: ParticleProcessMaterial
+
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		set_physics_process(false)
 		set_process(false)
+	
+	original_process_material = gpu_particles_2d.process_material.duplicate()
+
+func _exit_tree() -> void:
+	if tween != null:
+		tween.kill()
 
 func _process(_delta: float) -> void:
 	if gpu_particles_2d == null:
 		return
 	
-	health_per_centage = Dialogic.VAR.values.health/100.
-	gpu_particles_2d.process_material.color = Color.RED.lerp(Color.GREEN, health_per_centage)
+	update_portrait()
 
 func _get_covered_rect() -> Rect2:
 	return Rect2(gpu_particles_2d.position, Vector2(80, 80))
+
+var tween: Tween
+
+func update_portrait() -> void:
+	health_per_centage = Dialogic.VAR.values.health/100.
+	var process_material : ParticleProcessMaterial = gpu_particles_2d.process_material
+
+	tween = get_tree().create_tween()
+	tween.tween_property(process_material, "color", Color.RED.lerp(Color.GREEN, health_per_centage), .3)
+	tween.tween_property(process_material, "scale_min", original_process_material.scale_min * (1 + health_per_centage), .3)
+	tween.tween_property(process_material, "scale_max", original_process_material.scale_max * (1 + health_per_centage), .3)
